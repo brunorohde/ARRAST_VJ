04b38752ba75259344727d576e2307fb011d8dd9
Bruno Faria Rohde <brunorohde@avlinux8>
2017-08-31 00:43:43 -0400

    inclui módulo EXPORT

---
+++ Export/makeVideo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# [ARRAST_VJ] 1.0 - arrastvj.org
+#
+# Script para gerar vídeo a partir de frames e arquivo de áudio - utiliza ffmpeg
+#
+# por brunorohde - brunorohde.wordpress.com / github.com/brunorohde
+
+ffmpeg -r 24 -i "FRAME_%05d.jpg" -i AUDIO_MIX.wav -c:a copy -c:v mjpeg -q:v 0 Video.mov
